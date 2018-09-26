2faf495c2599372e7e7ab01fd55df8c7f7eac4d9
debrasia <ayee31@gmail.com>
2018-09-25 22:55:51 -0300

    Primer commit

---
+++ Assets/BounceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceManager : MonoBehaviour {
+
+    public GameObject Prefab;
+
+    public void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(Input.mousePosition.x);
+            Debug.Log(Input.mousePosition.y);
+            Vector3 v = new Vector3(0, 0, 3.5f);
+                Instantiate(Prefab, v, Quaternion.identity);
+        }
+    }
+}
